6716a39df385e9cf136b222d7628313ba804cda0
DANIV18 <igordaniv242@gmail.com>
2019-06-19 21:46:46 +0300

    Complete office class

--- office.cpp
+++ office.cpp
@@ -1,6 +1,82 @@
 #include "office.h"
+#include "employee.h"
 
-Office::Office()
+Office::Office(const std::string &name, const std::string &address, const double &area)
+    :mName{name},mAddress{address},mArea{area}
 {
 
+}
+
+std::string Office::name() const
+{
+    return mName;
+}
+
+void Office::setName(const std::string &name)
+{
+    mName = name;
+}
+
+std::string Office::address() const
+{
+    return mAddress;
+}
+
+void Office::setAddress(const std::string &address)
+{
+    mAddress = address;
+}
+
+
+int Office::employeeCount() const
+{
+    return static_cast<int>(mEmployees.size());
+}
+
+double Office::area() const
+{
+    return mArea;
+}
+
+void Office::setArea(double area)
+{
+    mArea = area;
+}
+
+void Office::addWorker(Employee *employee)
+{
+    if(employee != nullptr)
+    {
+        mEmployees.push_back(employee);
+    }
+}
+
+void Office::removeWorker(Employee *employee)
+{
+    for(std::list<Employee *>::iterator it=mEmployees.begin();
+        it != mEmployees.end(); ++it)
+    {
+        if(*it == employee)
+        {
+            mEmployees.erase(it);
+            break;
+        }
+    }
+}
+
+std::ostream &operator<<(std::ostream &out, const Office &office)
+{
+    out<<"name - "<<office.mName<<std::endl
+      <<"address - "<<office.mAddress<<std::endl
+     <<"area - "<<office.mArea<<std::endl
+    <<"employee count - "<<office.employeeCount()<<std::endl;
+    return out;
+}
+
+void Office::print() const
+{
+    std::cout<<"name - "<<mName<<std::endl
+            <<"address - "<<mAddress<<std::endl
+           <<"area - "<<mArea<<std::endl
+          <<"employee count - "<<employeeCount()<<std::endl;
 }
--- office.h
+++ office.h
@@ -1,11 +1,45 @@
 #ifndef OFFICE_H
 #define OFFICE_H
 
+#include <list>
+#include <string>
 
-class Office
+#include "printable.h"
+
+class Employee;
+class Office : public Printable
 {
 public:
-    Office();
+    Office(const std::string &name, const std::string &address,
+           const double &area);
+
+    std::string name() const;
+    void setName(const std::string &name);
+
+    std::string address() const;
+    void setAddress(const std::string &address);
+
+    int employeeCount() const;
+
+    double area() const;
+    void setArea(double area);
+
+    void addWorker(Employee * employee);
+
+    void removeWorker(Employee *employee);
+
+    friend std::ostream &operator<< (std::ostream &out, const Office &office);
+
+    // Printable interface
+public:
+    void print() const;
+
+private:
+    std::string mName;
+    std::string mAddress;
+    double mArea;
+    std::list<Employee *> mEmployees;
+
 };
 
 #endif // OFFICE_H
